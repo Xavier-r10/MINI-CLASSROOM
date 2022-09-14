a4914c3d934f50a936bda0cdc7459e3f319e6c63
xavier2001-sudo <xavierp20.aps1b@gmail.com>
2022-09-14 14:49:50 +0300

    add files

---
+++ assets/js/accueilProf.js
@@ -0,0 +1,11 @@
+let link = document.getElementById('link')
+let burger = 
+document.getElementById('burger')
+let ul = document.querySelector('ul')
+
+link.addEventListener('click', function(e)
+{
+    e.preventDefault()
+    burger.classList.toggle('open')
+    ul.classList.toggle('open')
+})
